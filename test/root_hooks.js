b81da3b51ab7fa5ff0964ea8083e6239aff0a2b1
Fer Hernandez <fh.1111@gmail.com>
2019-03-09 19:14:02 +0000

    Automatically backed up by Learn

--- test/root_hooks.js
+++ test/root_hooks.js
@@ -1,22 +1,22 @@
 "use strict";
-	
+
 	const chai = require('chai');
 	const app = require('../app');
 	const expect = chai.expect;
-	
+
 	let server;
-	
+
 	before(done => {
 	  return app.up().then(_server => {
 	    server = _server;
 	    done();
 	  });
 	});
-	
+
 	after(() => {
 	  server.close();
 	});
-	
+
 	describe('app', () => {
 	  describe('up', () => {
 	    it('is a function', () => {
